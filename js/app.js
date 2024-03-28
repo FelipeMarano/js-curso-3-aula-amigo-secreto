a7201eacd16a26728db2d90ab33e1fc671c93d6e
Felipe Marano <felipehenriquemarano@gmail.com>
2024-03-28 12:02:24 -0300

    add function adicionar

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function adicionar () {
+    let amigo = document.getElementById ("nome-amigo");
+    let lista = document.getElementById ("lista-amigos");
+    if (lista.textContent == "") { // quando for colocar o primeiro nome, fazer isso:
+        lista.textContent = amigo.value; // o texto da "lista" vai receber o valor do "amigo" (vai mudar o texto)
+    } else {
+        lista.textContent = lista.textContent + ", " + amigo.value; // quando for adicionar outro nome
+    }
+}
