e03b9c7cb552519b99afc58fea82f593ad9c2e3e
Felipe Marano <felipehenriquemarano@gmail.com>
2024-03-28 12:51:26 -0300

    finalizando a funcao sortear

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 let amigos = []; // criando um array para sortear os nomes
 
 function adicionar () {
-    let amigo = document.getElementById ("nome-amigo");
-    let lista = document.getElementById ("lista-amigos");
+    let amigo = document.getElementById ("nome-amigo"); // la no HTML
+    let lista = document.getElementById ("lista-amigos"); // la no HTML
     
     amigos.push (amigo.value); // para puxar o array e adicionar o "amigo" dentro dele
     
@@ -17,10 +17,19 @@ function adicionar () {
 
 function sortear () { 
     embaralha (amigos); // pegando a função "embaralha" da internet e usando ela na "sortear"
-
+    let sorteio = document.getElementById ("lista-sorteio"); // la no HMTL
+    for (let i = 0; i < amigos.length; i++) {  // para ir do começo (0) ate o final do "amigos" (i++ = para ir adicionando mais)
+        if (i == amigos.length -1) { // para verificar se ele chegou no final (para nao dar "undefined" no ultimo sorteado)
+            sorteio.innerHTML = sorteio.innerHTML + amigos [i] + "-->" + amigos [0] + "<br>";
+        } else { 
+           sorteio.innerHTML = sorteio.innerHTML + amigos [i] + "-->" + amigos [i + 1] + "<br>"; 
+            // para mostrar na tela quem sorteou quem // pega o primeiro amigo para o proximo da lista (ex:felipe --> ana)
+        }
+    }                                                           
+    
 }
 
-function embaralha(lista) { // função pegada na internet
+function embaralha(lista) { // função pegada na internet para embaralhar os nomes da lista
 
     for (let indice = lista.length; indice; indice--) {
 
