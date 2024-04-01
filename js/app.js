bcc72ff2ab1c71497c43ae73d31f9e7f2423b7f4
Felipe Marano <felipehenriquemarano@gmail.com>
2024-04-01 09:43:26 -0300

    final version

--- js/app.js
+++ js/app.js
@@ -1,62 +1,61 @@
-let amigos = []; // criando um array para sortear os nomes
+let amigos = []; 
 
 function adicionar () {
-    let amigo = document.getElementById ("nome-amigo"); // la no HTML
+    let amigo = document.getElementById ("nome-amigo");
     if (amigo.value == "") {
-        alert ("Informe o nome do amigo!"); // caso não tenha nome, dar um alerta
-        return; // para não executar as funções abaixo
+        alert ("Informe o nome do amigo!"); 
+        return; 
     }
     
-    if (amigos.includes(amigo.value)) {
-        alert ("Nome já adicionado!"); // caso o nome ja tenha sido adicionado, dar um alerta
-        return; // para não executar as funções abaixo
+    if (amigos.includes(amigo.value)) { 
+        alert ("Nome já adicionado!"); 
+        return; 
     }
 
-    let lista = document.getElementById ("lista-amigos"); // la no HTML
+    let lista = document.getElementById ("lista-amigos"); 
     
-    amigos.push (amigo.value); // para puxar o array e adicionar o "amigo" dentro dele
+    amigos.push (amigo.value); 
     
-    if (lista.textContent == "") { // quando for colocar o primeiro nome, fazer isso:
-        lista.textContent = amigo.value; // o texto da "lista" vai receber o valor do "amigo" (vai mudar o texto)
+    if (lista.textContent == "") { 
+        lista.textContent = amigo.value; 
     } else {
-        lista.textContent = lista.textContent + ", " + amigo.value; // quando for adicionar outro nome
+        lista.textContent = lista.textContent + ", " + amigo.value;
     }
 
-    amigo.value = "" // para limpar e colocar outro nome
+    amigo.value = "" 
 }
 
 function sortear () { 
-    if (amigos.length <= 4) { // se o numero de "amigos for menor que 4"
-        alert ("Adicione pelo menos 4 amigos!"); // caso tenha menos que 4 amigos, alertar
-        return; // para nao executar nenhuma função abaixo caso tenha menos que 4 amigos
+    if (amigos.length <= 4) { 
+        alert ("Adicione pelo menos 4 amigos!"); 
+        return; 
     }
     
-    embaralha (amigos); // pegando a função "embaralha" da internet e usando ela na "sortear"
-    let sorteio = document.getElementById ("lista-sorteio"); // la no HMTL
-    for (let i = 0; i < amigos.length; i++) {  // para ir do começo (0) ate o final do "amigos" (i++ = para ir adicionando mais)
-        if (i == amigos.length -1) { // para verificar se ele chegou no final (para nao dar "undefined" no ultimo sorteado)
+    embaralha (amigos); 
+    let sorteio = document.getElementById ("lista-sorteio"); 
+    for (let i = 0; i < amigos.length; i++) {  
+        if (i == amigos.length -1) { 
             sorteio.innerHTML = sorteio.innerHTML + amigos [i] + "-->" + amigos [0] + "<br>";
         } else { 
            sorteio.innerHTML = sorteio.innerHTML + amigos [i] + "-->" + amigos [i + 1] + "<br>"; 
-            // para mostrar na tela quem sorteou quem // pega o primeiro amigo para o proximo da lista (ex:felipe --> ana)
         }
     }                                                           
     
 }
 
-function embaralha(lista) { // função pegada na internet para embaralhar os nomes da lista
+function embaralha(lista) {
 
     for (let indice = lista.length; indice; indice--) {
 
         const indiceAleatorio = Math.floor(Math.random() * indice);
 
-        // atribuição via destructuring
+       
         [lista[indice - 1], lista[indiceAleatorio]] = 
             [lista[indiceAleatorio], lista[indice - 1]];
     }
 }
 
-function reiniciar () { // fazer todas as funções para vazio para reiniciar
+function reiniciar () { 
     amigos = [];
     document.getElementById ("lista-amigos") = "";
     document.getElementById ("lista-sorteio") = "";
